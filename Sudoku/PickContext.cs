7628d0d5e3c819849f4a2ef77aef729b30d8dc57
Adrian Savage <savage_adrian@hotmail.com>
2015-08-29 19:53:06 +0100

    Centre grid in form

--- Sudoku/PickContext.cs
+++ Sudoku/PickContext.cs
@@ -8,7 +8,7 @@ namespace Sudoku
 {
     public class PickContext
     {
-        public readonly int scw, sch;
+        public readonly int scw, sch, ox, oy;
         public readonly Size FullSize;
 
         public PickContext(Size size)
@@ -16,12 +16,14 @@ namespace Sudoku
             FullSize = size;
             scw = size.Width / 27;
             sch = size.Height / 27;
+            ox = (size.Width - scw * 27) / 2;
+            oy = (size.Height - sch * 27) / 2;
         }
 
         public Size SubcellSize { get { return new Size(scw, sch); } }
         public Size CellSize { get { return new Size(scw * 3, sch * 3); } }
-        public Point CellLocation(int x, int y) { return new Point(scw * 3 * x, sch * 3 * y); }
-        public Point SubcellLocation(int x, int y) { return new Point(scw * x, sch * y); }
+        public Point CellLocation(int x, int y) { return new Point(ox+scw * 3 * x, oy+sch * 3 * y); }
+        public Point SubcellLocation(int x, int y) { return new Point(ox+scw * x, oy+sch * y); }
 
     }
 }
